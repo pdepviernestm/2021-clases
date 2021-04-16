5642375759d12b1e677c6d3244466582f21eaaeb
juanFdS <juan.fernandes@10pines.com>
2021-04-16 11:03:26 -0300

    Subida solucion de preguntar

--- clase-02/src/Library.hs
+++ clase-02/src/Library.hs
@@ -3,7 +3,28 @@ import PdePreludat
 import Data.List (isPrefixOf, isSuffixOf)
 
 preguntar :: String -> String
-preguntar oracion = implementame
+preguntar oracion =
+    agregarInterrogacionAlPrincipio (agregarInterrogacionAlFinal oracion)
+
+leFaltaSignoDePreguntaAlFinal :: String -> Bool
+leFaltaSignoDePreguntaAlFinal oracion = not ("?" `isSuffixOf` oracion)
+
+agregarInterrogacionAlFinal :: String -> String
+agregarInterrogacionAlFinal oracion
+    | leFaltaSignoDePreguntaAlFinal oracion = oracion ++ "?"
+    | otherwise = oracion
+
+leFaltaSignoDePreguntaAlPrincipio :: String -> Bool
+leFaltaSignoDePreguntaAlPrincipio oracion = not ("¿" `isPrefixOf` oracion)
+
+agregarInterrogacionAlPrincipio :: String -> String
+agregarInterrogacionAlPrincipio oracion
+    | leFaltaSignoDePreguntaAlPrincipio oracion = "¿" ++ oracion
+    | otherwise = oracion
+
+-- ¿Y como implementaríamos ahora exclamar pero sin repetir lógica?
+-- exclamar "hola" -> "¡hola!"
+-- exclamar "hola!" -> "¡hola!"
 
 -- isPrefixOf nos dice si un string empieza con otro string
 -- isSuffixOf nos dice si un string termina con otro string
--- clase-02/src/Spec.hs
+++ clase-02/src/Spec.hs
@@ -6,7 +6,12 @@ import Test.Hspec
 correrTests :: IO ()
 correrTests = hspec $ do
   describe "Tests de clase 2" $ do
-    it "test de mentira" $ do
-      1 + 1 `shouldBe` 3
-    
+    it "si le paso a preguntar una oracion sin signos de pregunta, se los agrega" $ do
+      preguntar "hola" `shouldBe` "¿hola?"
+    it "si le paso a preguntar una oracion con signo de pregunta solo al final, le agrega el del principio" $ do
+      preguntar "como estas?" `shouldBe` "¿como estas?"
+    it "si le paso a preguntar una oracion con signo de pregunta solo al principio, le agrega el del final" $ do
+      preguntar "¿como estas" `shouldBe` "¿como estas?"
+    it "si le paso a preguntar una oracion con signos de pregunta, la deja igual" $ do
+      preguntar "¿como estas?" `shouldBe` "¿como estas?"
     
