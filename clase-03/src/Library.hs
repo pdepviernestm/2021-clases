852116c96b788c5ac5020d39f551c221cd8603d0
fecheromero <fecheromero@gmail.com>
2021-04-22 20:49:48 -0300

    Agrego regla de juego despues del +4

--- clase-03/src/Library.hs
+++ clase-03/src/Library.hs
@@ -8,3 +8,5 @@ import PdePreludat
 -- Las cartas de reversa o saltear turno que también tienen color, se pueden jugar sobre otra carta de la misma figura o del mismo color.
 
 -- Finalmente, las cartas de +4 cambian el color así que se pueden jugar sobre cualquier otra carta.
+
+-- Luego de un  +4  se puede jugar otro +4 o cualquier carta que coincida en color
