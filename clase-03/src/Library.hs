00a05c399fd4505363a9f698c07b5d8e255af909
feche <federico.romero@10pines.com>
2021-04-23 12:38:11 -0300

    ejercicio terminado en clase

--- clase-03/src/Library.hs
+++ clase-03/src/Library.hs
@@ -1,27 +1,50 @@
 module Library where
 import PdePreludat
 
-rojo = "Rojo"
-verde = "Verde"
-azul = "Azul"
-amarillo = "Amarillo"
+rojo = Rojo
+verde = Verde
+azul = Azul
+amarillo = Amarillo
 -- Queremos saber si una carta del Uno se puede jugar luego de otra.
-unoRojo = (1,rojo)
-dosVerde=(2,verde)
-tresRojo=(3,rojo)
+unoRojo = CartaNumerica 1 rojo
+dosVerde= CartaNumerica 2 verde
+tresRojo= CartaNumerica 3 rojo
 
-unoAmarillo=(1,amarillo)
+unoAmarillo= CartaNumerica 1 amarillo
 
-type Carta = (Number,String)
+reversaRojo= CartaEspecial Reversa Rojo
+
+data Carta = CartaNumerica{
+    numero :: Number,
+    color ::    Color 
+} | CartaEspecial {
+    dibujo ::Dibujo,
+    color:: Color
+} deriving(Show,Eq)
 
 sePuedeJugar:: Carta -> Carta -> Bool 
 sePuedeJugar unaCarta cartaDelMazo = compartenValor unaCarta cartaDelMazo  || compartenColor unaCarta cartaDelMazo
 
+
 compartenValor:: Carta -> Carta -> Bool
-compartenValor unaCarta otraCarta = fst unaCarta == fst otraCarta
+compartenValor (CartaNumerica valorDeCarta _) (CartaNumerica valorDeOtraCarta _) = valorDeCarta == valorDeOtraCarta
+compartenValor (CartaEspecial dibujoCarta _) (CartaEspecial dibujoOtraCarta _) = dibujoCarta == dibujoOtraCarta
+compartenValor _ _ = False
 
 compartenColor:: Carta -> Carta -> Bool
-compartenColor unaCarta otraCarta = snd unaCarta == snd otraCarta
+compartenColor unaCarta otraCarta = color unaCarta == color otraCarta
+
+data Color = Rojo | Verde | Amarillo | Azul deriving(Show,Eq)
+
+data Dibujo = Reversa | SaltarTurno deriving(Show,Eq)
+
+juan :: (Number, [Char])
+juan = (20, "juan")
+
+
+
+
+
 -- Las cartas que tienen numero y color,  =se pueden jugar sobre cartas que tengan el mismo numero o el mismo color.
 
 -- Las cartas de reversa o saltear turno que también tienen color, se pueden jugar sobre otra carta de la misma figura o del mismo color.
--- clase-03/src/Spec.hs
+++ clase-03/src/Spec.hs
@@ -13,4 +13,18 @@ correrTests = hspec $ do
     it "una carta se puede jugar si tiene el mismo numero que otra" $ do
       sePuedeJugar unoRojo  unoAmarillo    `shouldBe`  True 
     it "una carta  no se puede jugar si no comparte ni numero ni color con la del mazo" $ do
-      sePuedeJugar unoRojo  (2,verde)     `shouldBe`  False 
+      sePuedeJugar unoRojo  dosVerde      `shouldBe`  False 
+    it "una carta  especial se puede jugar despues de si misma" $ do
+      sePuedeJugar reversaRojo   reversaRojo       `shouldBe`  True 
+    it "una carta  especial se puede jugar despues de una carta especial que comparte dibujo" $ do
+      sePuedeJugar reversaRojo   (CartaEspecial  Reversa Amarillo )       `shouldBe`  True
+    it "una carta  especial  se puede jugar despues de una carta especial que comparte color" $ do
+      sePuedeJugar reversaRojo   (CartaEspecial  SaltarTurno  Rojo )       `shouldBe`  True
+    it "una carta  especial  no se puede jugar despues de una carta especial que no comparte color ni dibujo" $ do
+      sePuedeJugar reversaRojo   (CartaEspecial  SaltarTurno  Amarillo  )       `shouldBe`  False
+    it "una carta  numerica  no se puede jugar despues de una carta especial que no comparte color ni dibujo" $ do
+      sePuedeJugar unoRojo    (CartaEspecial  SaltarTurno  Amarillo  )       `shouldBe`  False
+    it "una carta  especial   se puede jugar despues de una carta numerica que comparte color" $ do
+      sePuedeJugar reversaRojo   (CartaNumerica  2  Rojo  )       `shouldBe`  True
+    it "una carta  numerica   se puede jugar despues de una carta especial que comparte color" $ do
+      sePuedeJugar (CartaNumerica  2  Rojo  ) reversaRojo          `shouldBe`  True
