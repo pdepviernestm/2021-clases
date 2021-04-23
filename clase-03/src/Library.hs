ebb507e28b5d8c9731cef1653f75fe5a8919918e
feche <federico.romero@10pines.com>
2021-04-23 10:40:16 -0300

    primera version del ejercicio de cartas antes del recreo

--- clase-03/src/Library.hs
+++ clase-03/src/Library.hs
@@ -1,12 +1,35 @@
 module Library where
 import PdePreludat
 
+rojo = "Rojo"
+verde = "Verde"
+azul = "Azul"
+amarillo = "Amarillo"
 -- Queremos saber si una carta del Uno se puede jugar luego de otra.
+unoRojo = (1,rojo)
+dosVerde=(2,verde)
+tresRojo=(3,rojo)
 
--- Las cartas que tienen numero y color, se pueden jugar sobre cartas que tengan el mismo numero o el mismo color.
+unoAmarillo=(1,amarillo)
+
+type Carta = (Number,String)
+
+sePuedeJugar:: Carta -> Carta -> Bool 
+sePuedeJugar unaCarta cartaDelMazo = compartenValor unaCarta cartaDelMazo  || compartenColor unaCarta cartaDelMazo
+
+compartenValor:: Carta -> Carta -> Bool
+compartenValor unaCarta otraCarta = fst unaCarta == fst otraCarta
+
+compartenColor:: Carta -> Carta -> Bool
+compartenColor unaCarta otraCarta = snd unaCarta == snd otraCarta
+-- Las cartas que tienen numero y color,  =se pueden jugar sobre cartas que tengan el mismo numero o el mismo color.
 
 -- Las cartas de reversa o saltear turno que también tienen color, se pueden jugar sobre otra carta de la misma figura o del mismo color.
 
 -- Finalmente, las cartas de +4 cambian el color así que se pueden jugar sobre cualquier otra carta.
 
 -- Luego de un  +4  se puede jugar otro +4 o cualquier carta que coincida en color
+
+
+
+
--- clase-03/src/Spec.hs
+++ clase-03/src/Spec.hs
@@ -6,5 +6,11 @@ import Test.Hspec
 correrTests :: IO ()
 correrTests = hspec $ do
   describe "Tests de clase 3" $ do
-    it "" $ do
-      2 + 2 `shouldBe` 4
+    it "una carta se puede jugar si es igual a otra " $ do
+      sePuedeJugar unoRojo  unoRojo  `shouldBe`  True 
+    it "una carta se puede jugar si tiene el mismo color que otra" $ do
+      sePuedeJugar unoRojo  tresRojo   `shouldBe`  True 
+    it "una carta se puede jugar si tiene el mismo numero que otra" $ do
+      sePuedeJugar unoRojo  unoAmarillo    `shouldBe`  True 
+    it "una carta  no se puede jugar si no comparte ni numero ni color con la del mazo" $ do
+      sePuedeJugar unoRojo  (2,verde)     `shouldBe`  False 
