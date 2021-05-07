a768a8f1bc1f0b8310642c14030e3759d1a22b4f
juanFdS <juan.fernandes@10pines.com>
2021-05-06 23:27:49 -0300

    Codigo para implementar en clase

--- clase-05/src/Library.hs
+++ clase-05/src/Library.hs
@@ -32,3 +32,55 @@ esFigura :: Carta -> Bool
 esFigura (CartaEspecial _ _) = True
 esFigura _ = True 
 
+-- Para implementar en clase
+
+-- >>> aplicarDosVeces tail [1,2,3,4,5]
+-- [3,4,5]
+-- >>> aplicarDosVeces (\x -> x + 3) 5
+-- 11
+aplicarDosVeces = implementame
+
+-- >>> aplicarEnOrden tail length [1,2,3,4,5]
+-- 4
+-- >>> aplicarEnOrden not esFigura (CartaNumerica 5 Azul)
+-- False
+aplicarEnOrden = implementame
+
+-- >>> positivos [-1,2,-4,5]
+-- [2, 5]
+positivos :: [Number] -> [Number]
+positivos = implementame
+
+-- >>> noHayNingunoMayorQue 5 [1,2,3]
+-- True
+-- >>> noHayNingunoMayorQue 2 [1,2,3]
+-- False
+noHayNingunoMayorQue :: Number -> [Number] -> Bool
+noHayNingunoMayorQue maximo = implementame
+
+-- >>> tieneEspacio "hola"
+-- False
+-- >>> tieneEspacio "hola mundo"
+-- True
+tieneEspacio :: String -> Bool
+tieneEspacio = implementame
+
+-- >>> sumatoria [1,2,3,4,5]
+-- 15
+sumatoria :: [Number] -> Number
+sumatoria numeros = implementame
+
+-- >>> concatenarTodas ["pero el ", "te", "ma to", "davia da", " para ", "mas"]
+-- "pero el tema todavia da para mas"
+concatenarTodas :: [String] -> String
+concatenarTodas strings = implementame
+
+-- >>> cumpleTodas [esRoja, esFigura] (CartaEspecial Reversa Roja)
+-- True
+-- >>> cumpleTodas [(\carta -> esDeColor Verde carta), esFigura] (CartaEspecial Reversa Roja)
+-- False
+--
+-- (es la funcion all)
+--
+cumpleTodas :: [a -> Bool] -> a -> Bool
+cumpleTodas condiciones valor = implementame
